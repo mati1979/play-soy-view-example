ff3e32a8f96c6b9fb03517f8706e24a96d8a4456
Mateusz Szczap <mateusz.szczap@gmail.com>
2014-08-30 21:37:04 +0200

    play 2.4

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,16 @@
 package controllers;
 
 import com.github.mati1979.play.soyplugin.plugin.Soy;
+import model.IndexPageModel;
 import pagelets.HeaderModel;
 import pagelets.HeaderPagelet;
 import pagelets.WordsModel;
 import pagelets.WordsPagelet;
 import play.libs.F;
-import play.libs.ws.WSClient;
-import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Html;
+import utils.Promises;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -27,18 +27,12 @@ public class Application extends Controller {
     @Inject
     private WordsPagelet wordsPagelet;
 
-    @Inject
-    private WSClient wsClient;
-
     public F.Promise<Result> index() {
         final F.Promise<HeaderModel> headerModelP = headerPagelet.invoke();
         final F.Promise<WordsModel> wordsModelP = wordsPagelet.invoke();
-        final F.Promise<WSResponse> responsePromise = wsClient.url("http://www.onet.pl/").get();
 
-//        return Promises.zip(headerModelP, wordsModelP, (header, words)
-//                -> ok(Html.apply(soy.html("pages.index", new IndexPageModel(header, words)))));
-
-        return responsePromise.map(response -> ok(Html.apply(response.getBody())));
+        return Promises.zip(headerModelP, wordsModelP, (header, words)
+                -> ok(Html.apply(soy.html("pages.index", new IndexPageModel(header, words)))));
     }
 
 }
--- app/soy/PlaySoyConfigExt.java
+++ app/soy/PlaySoyConfigExt.java
@@ -8,13 +8,17 @@ import com.github.mati1979.play.soyplugin.ajax.auth.PermissableAuthManager;
 import com.github.mati1979.play.soyplugin.ajax.hash.HashFileGenerator;
 import com.github.mati1979.play.soyplugin.ajax.hash.MD5HashFileGenerator;
 import com.github.mati1979.play.soyplugin.ajax.process.DefaultOutputProcessors;
+import com.github.mati1979.play.soyplugin.ajax.process.OutputProcessor;
 import com.github.mati1979.play.soyplugin.ajax.process.OutputProcessors;
+import com.github.mati1979.play.soyplugin.ajax.process.google.GoogleClosureOutputProcessor;
 import com.github.mati1979.play.soyplugin.ajax.runtime.SoyHashesRuntimeDataResolver;
 import com.github.mati1979.play.soyplugin.bundle.DefaultSoyMsgBundleResolver;
 import com.github.mati1979.play.soyplugin.bundle.SoyMsgBundleResolver;
 import com.github.mati1979.play.soyplugin.compile.DefaultTofuCompiler;
 import com.github.mati1979.play.soyplugin.compile.TofuCompiler;
-import com.github.mati1979.play.soyplugin.config.DefaultSoyViewConf;
+import com.github.mati1979.play.soyplugin.config.ConfigDefaults;
+import com.github.mati1979.play.soyplugin.config.DefaultConfigDefaults;
+import com.github.mati1979.play.soyplugin.config.PlaySoyViewConf;
 import com.github.mati1979.play.soyplugin.config.SoyViewConf;
 import com.github.mati1979.play.soyplugin.data.ReflectionToSoyDataConverter;
 import com.github.mati1979.play.soyplugin.data.ToSoyDataConverter;
@@ -62,26 +66,31 @@ public class PlaySoyConfigExt extends AbstractModule {
         bind(SoyMsgBundleResolver.class).to(DefaultSoyMsgBundleResolver.class).in(Singleton.class);
         bind(CompiledTemplatesHolder.class).to(DefaultCompiledTemplatesHolder.class).in(Singleton.class);
         bind(TemplateRenderer.class).to(DefaultTemplateRenderer.class).in(Singleton.class);
+        bind(Soy.class).to(DefaultSoy.class).in(Singleton.class);
+        bind(ConfigDefaults.class).to(DefaultConfigDefaults.class).in(Singleton.class);
+        bind(SoyViewConf.class).to(PlaySoyViewConf.class).in(Singleton.class);
+
+
         bind(SoyAllowedUrlsResolver.class).to(DefaultSoyAllowedUrlsResolver.class).in(Singleton.class);
         bind(HashFileGenerator.class).to(MD5HashFileGenerator.class).in(Singleton.class);
         bind(AuthManager.class).to(PermissableAuthManager.class).in(Singleton.class);
+        bind(OutputProcessor.class).annotatedWith(Names.named("google")).to(GoogleClosureOutputProcessor.class);
         bind(OutputProcessors.class).to(DefaultOutputProcessors.class).in(Singleton.class);
         bind(SoyAjaxController.class).in(Singleton.class);
         bind(LocaleProvider.class).to(PlayLocaleProvider.class).in(Singleton.class);
-        bind(Soy.class).to(DefaultSoy.class).in(Singleton.class);
         bind(RuntimeDataResolver.class).annotatedWith(Names.named("soyHashes")).to(SoyHashesRuntimeDataResolver.class).in(Singleton.class);
         bind(RuntimeDataResolver.class).annotatedWith(Names.named("assets")).to(AssetsRuntimeResolver.class).in(Singleton.class);
         bind(RuntimeDataResolver.class).annotatedWith(Names.named("loggedIn")).to(LoggedInRuntimeResolver.class).in(Singleton.class);
     }
 
-    @Provides
-    @Singleton
-    SoyViewConf soyViewConff() {
-        return DefaultSoyViewConf.Builder.newBuilder()
-                .withAjaxAllowedUrls("template2.soy")
-                .withAjaxSecurityEnabled(true)
-                .build();
-    }
+//    @Provides
+//    @Singleton
+//    SoyViewConf soyViewConff() {
+//        return DefaultSoyViewConf.Builder.newBuilder()
+//                .withAjaxAllowedUrls("template2.soy")
+//                .withAjaxSecurityEnabled(true)
+//                .build();
+//    }
 
     @Provides
     @Singleton
